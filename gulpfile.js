d786d618433d080f526b7b5f32a124a9fc27f754
mduraes1994 <mduraes1994@gmail.com>
2017-11-05 23:17:20 -0200

    Finish gulp tasks and npm script

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,35 @@ const ts = require('gulp-typescript');
 
 const tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('scripts', () => {
+gulp.task('scripts', ['static'], () => {
 
     const tsResult = tsProject.src()
         .pipe(tsProject());
     
-    tsResult.js
+    return tsResult.js
         .pipe(gulp.dest('dist'));
 
 });
+
+
+gulp.task('static', ['clean'], () => {
+    return gulp
+        .src(['src/**/*.json'])
+        .pipe(gulp.dest('dist'));
+
+});
+
+
+gulp.task('clean', () => {
+    return gulp
+        .src('dist')
+        .pipe(clean());
+});
+
+gulp.task('build', ['scripts']);
+
+gulp.task('watch', ['build'], () => {
+    return gulp.watch(['src/**/*.ts', 'src/**/*.json'], ['build']);
+});
+
+gulp.task('default', ['watch']);

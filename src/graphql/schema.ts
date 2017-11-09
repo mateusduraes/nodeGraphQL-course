eb1a1a5f06964550fb1c0e231539e074ba17f881
mduraes1994 <mduraes1994@gmail.com>
2017-11-09 00:58:12 -0200

    Remove trivial resolvers - not necessary

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -31,11 +31,6 @@ const typeDefs = `
 `;
 
 const resolvers = {
-    User: {
-        id: (user) => user.id,
-        name: (user) => user.name,
-        email: (user) => user.email
-    },
 
     Query: {
         allUsers: () => usersMock

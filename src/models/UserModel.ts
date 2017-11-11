ce7fe0cb1c4584e059cb775e325db2f1529ed8df
mduraes1994 <mduraes1994@gmail.com>
2017-11-11 00:25:17 -0200

    Almost finish UserModel

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -14,3 +14,41 @@ export interface UserInstance extends Sequelize.Instance<UserAttributes>, UserAt
 }
 
 export interface UserModel extends BaseModelInterface, Sequelize.Model<UserInstance, UserAttributes> {}
+
+export default (sequelize: Sequelize.Sequelize, DataTypes: Sequelize.DataTypes): UserModel => {
+    
+    const User: UserModel = 
+        sequelize.define('User', {
+            id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true
+            },
+            name: {
+                type: DataTypes.STRING(128),
+                allowNull: false
+            },
+            email: {
+                type: DataTypes.STRING(128),
+                allowNull: false,
+                unique: true
+            },
+            password: {
+                type: DataTypes.STRING(128),
+                allowNull: false,
+                validate: {
+                    notEmpty: true
+                }
+            },
+            photo: {
+                type: DataTypes.BLOB({
+                    length: 'long'
+                }),
+                allowNull: true,
+                defaultValue: null
+            }
+        });
+        return User;
+
+};

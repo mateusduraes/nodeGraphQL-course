1c3d6aa3be8c6e91db6e255685694197f1fab27d
mduraes1994 <mduraes1994@gmail.com>
2017-11-11 00:35:47 -0200

    Finishing UserModel

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,12 +1,18 @@
+import { ModelsInterface } from './../interfaces/ModelsInterface';
 import { UserAttributes, UserInstance } from './UserModel';
+import { genSaltSync, hashSync, compareSync } from 'bcryptjs';
+
 import { BaseModelInterface } from './../interfaces/BaseModelInterface';
 import * as  Sequelize from 'sequelize';
+
 export interface UserAttributes {
     id?: number;
     name?: string;
     email?: string;
     password?: string;
     photo?: string;
+    createdAt?: string; // This two properties are created by sequelize
+    updateAt?: string;
 }
 
 export interface UserInstance extends Sequelize.Instance<UserAttributes>, UserAttributes {
@@ -52,10 +58,19 @@ export default (sequelize: Sequelize.Sequelize, DataTypes: Sequelize.DataTypes):
             tableName: 'users',
             hooks: {
                 beforeCreate: (user: UserInstance, options: Sequelize.CreateOptions): void => {
-                    
+                    const salt = genSaltSync();
+                    user.password = hashSync(user.password, salt);                    
                 }
             }
         });
+
+        // In case of associates the code will look like this...
+        User.associate = (models: ModelsInterface): void => {};
+
+        User.prototype.isPassword = (encodedPassword: string, password: string): boolean => {
+            return compareSync(password, encodedPassword);
+        };
+
         return User;
 
 };

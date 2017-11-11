6c7dc09ba31219d7a6bfab60a4f68ce37a4f53af
mduraes1994 <mduraes1994@gmail.com>
2017-11-11 00:18:18 -0200

    UserModel interface

--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -1,3 +1,6 @@
+import { UserAttributes, UserInstance } from './UserModel';
+import { BaseModelInterface } from './../interfaces/BaseModelInterface';
+import * as  Sequelize from 'sequelize';
 export interface UserAttributes {
     id?: number;
     name?: string;
@@ -5,3 +8,9 @@ export interface UserAttributes {
     password?: string;
     photo?: string;
 }
+
+export interface UserInstance extends Sequelize.Instance<UserAttributes>, UserAttributes {
+    isPassword(encodedPassword: string, password: string): boolean;
+}
+
+export interface UserModel extends BaseModelInterface, Sequelize.Model<UserInstance, UserAttributes> {}

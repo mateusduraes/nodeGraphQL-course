55825c36c2bad3e6e9f89d6e7582e350443f9e46
mduraes1994 <mduraes1994@gmail.com>
2017-11-11 00:37:11 -0200

    PostAttributes

---
+++ src/models/PostModel.ts
@@ -0,0 +1,10 @@
+export interface PostAttributes {    
+    id?: number;
+    title?: string;
+    content?: string;
+    photo?: string;
+    author?: number;
+    createdAt?: string;
+    updateAt?: string;
+}
+

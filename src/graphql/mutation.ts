12f0e4d15537e05555345aeb3f7f6b04e7b32637
mduraes1994 <mduraes1994@gmail.com>
2017-11-12 02:27:48 -0200

    User Schema

--- src/graphql/mutation.ts
+++ src/graphql/mutation.ts
@@ -1,6 +1,8 @@
+import { userMutations } from './resources/user/user.schema';
+
 const Mutation: string = `
     type Mutation {
-
+        ${userMutations}
     }
 `;
 
--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -1,6 +1,8 @@
+import { userQueries } from './resources/user/user.schema';
+
 const Query: string = `
     type Query {
-
+        ${userQueries}
     }
 `;
 
